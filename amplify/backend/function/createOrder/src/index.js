75c5f8ca6b97fd6ab4682bd64ac56ed01900f48d
Dhruv Doshi <dhruvdoshi25071999@gmail.com>
2021-12-09 20:27:17 -0400

    updated create order lambda function

--- amplify/backend/function/createOrder/src/index.js
+++ amplify/backend/function/createOrder/src/index.js
@@ -1,15 +1,78 @@
+const { v4: uuidv4 } = require("uuid");
+const AWS = require("aws-sdk");
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
+const ORDER_TABLE = "<order_table_name>";
+const ORDER_TYPE = "Order";
+const EVENT_ORDER_TABLE = "<event_order_table name>";
+const EVENT_ORDER_TYPE = "EventOrder";
 
+const createOrder = async (payload) => {
+  const { order_id, username, email, total } = payload;
+  var params = {
+    TableName: ORDER_TABLE,
+    Item: {
+      id: order_id,
+      __typename: ORDER_TYPE,
+      customer: email,
+      user: username,
+      total: total,
+      updatedAt: new Date().toISOString(),
+      createdAt: new Date().toISOString()
+    }
+  };
+  console.log(params);
+  await documentClient.put(params).promise();
+};
+
+const createEventOrder = async (payload) => {
+  let eventOrders = [];
+  for (i = 0; i < payload.cart.length; i++) {
+    const cartItem = payload.cart[i];
+    eventOrders.push({
+      PutRequest: {
+        Item: {
+          id: uuidv4(),
+          __typename: EVENT_ORDER_TYPE,
+          event_id: cartItem.id,
+          order_id: payload.order_id,
+          customer: payload.email,
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString()
+        }
+      }
+    });
+  }
+  let params = {
+    RequestItems: {}
+  };
+  params["RequestItems"][EVENT_ORDER_TABLE] = eventOrders;
+  console.log(params);
+  await documentClient.batchWrite(params).promise();
+};
+
+/*
+ * Get order details from processPayment lambda
+ * Create an order
+ * Link events to the order - Users can see the past orders and admins can view orders by user
+ * Email the invoice (Will be added later)
+ */
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  try {
+    let payload = event.prev.result;
+    payload.order_id = uuidv4();
+
+    // create a new order
+    await createOrder(payload);
+
+    // links events with the order
+    await createEventOrder(payload);
+
+    // Note - You may add another function to email the invoice to the user
+
+    return "SUCCESS";
+  } catch (err) {
+    console.log(err);
+    return new Error(err);
+  }
 };
